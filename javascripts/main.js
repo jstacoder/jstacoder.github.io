e34deb37340fb15a36ae25cf0a22deb9922de53e
Kyle J. Roux <kyle@level2designs.com>
2015-09-08 18:56:57 -0700

    Replace master branch with page content via GitHub

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,1 @@
 console.log('This would be the main JS file.');
-
-var btn = document.querySelectorAll('#sidebar > a')[0];
-btn.onClick = function(e){
-    e.preventDefault();
-    btn.remove();
-};
