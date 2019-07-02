c4ea1bbe42289bbb7671bcf1249287f9d0b46fff
Kyle J. Roux <jstacoder@gmail.com>
2019-07-02 13:35:59 -0700

    Update task-checkbox.jsx

--- content/posts/2016-06-30-react-hooks-tasklist/task-checkbox.jsx
+++ content/posts/2016-06-30-react-hooks-tasklist/task-checkbox.jsx
@@ -1,13 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   BorderBox,
   StyledOcticon
 } from '@primer/components'
+import PropTypes from 'prop-types'
+
 import { Check } from '@primer/octicons-react'
 
+const DefaultIcon = StyledOcticon 
 
-export default ({checked}) => (
-  <BorderBox textAlign={'center'} size={20} mr={2}>
-    {checked ? <StyledOcticon color={"green.6"} ml={1} verticalAlign={'text-top'} icon={Check} size={15}/> : null}
+const defaultIconProps = {
+  color:"green.6",
+  ml:1,
+  verticalAlign:'text-top',
+  icon:Check,
+  size:.75*size
+}
+
+const CustomCheckbox = ({
+  checked, 
+  onChange, 
+  defaultChecked,
+  size,
+  icon:IconComponent,
+  iconProps,
+  ...props
+  }) => {
+  const [stateValue, setStateValue] = useState(defaultChecked)
+  
+  const value = checked !== undefined ? checked : stateValue
+  
+  const innerChangeValue = e => {
+    setStateValue(!value)
+  }
+  
+  const onChangeValue = onChange !== undefined ? onChange : innerChangeValue
+  
+  return (
+  <BorderBox 
+    {...props}
+    onClick={onChangeValue}
+    textAlign={'center'} 
+    size={size} 
+    mr={2}>
+    {value ? 
+     <IconComponent {...iconProps}/>: 
+      null
+     }
   </BorderBox>
-)
+  )
+}
+
+export default CustomCheckbox
+
+CustomCheckbox.propTypes = {
+  /** pass for controlled component */
+  checked: PropTypes.boolean,
+  /** also used to create controlled component */
+  onChange: PropTypes.func,
+  /** pass to let component handle state */
+  defaultChecked: PropTypes.boolean,
+  /** pass to change size */
+  size: PropTypes.number,
+  /** pass to change default icon */
+  icon: PropTypes.Component,
+  /** props to pass icon */
+  iconProps: PropTypes.any,
+}
+
+CustomCheckbox.defaultProps = {
+  size: 20,
+  icon: DefaultIcon,
+  iconProps: defaultIconProps,
+}
