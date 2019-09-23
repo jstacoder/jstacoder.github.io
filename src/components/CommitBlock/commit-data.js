34c8cb3c13346fe9568ea3bdf08efc37cbf020e3
kyle roux <kyle@casepeer.com>
2019-09-23 15:10:36 -0700

    h

--- src/components/CommitBlock/commit-data.js
+++ src/components/CommitBlock/commit-data.js
@@ -1,6 +1,8 @@
 export const commitData = [
   {
     message: 'Update main.yml',
+    authoredDate: '2019-09-19T16:18:44Z',
+    committedDate: '2019-09-19T16:18:44Z',
     author: {
       avatarUrl: 'https://avatars2.githubusercontent.com/u/4082254?v=4',
       user: {
@@ -12,12 +14,12 @@ export const commitData = [
       'https://github.com/jstacoder/jstacoder.github.io/tree/98e9ab4b269c7de3a87aa4a7c38ffa18601eb8d1',
     commitUrl:
       'https://github.com/jstacoder/jstacoder.github.io/commit/98e9ab4b269c7de3a87aa4a7c38ffa18601eb8d1',
-    authoredDate: '2019-09-19T16:18:44Z',
-    committedDate: '2019-09-19T16:18:44Z',
     commitSha: '98e9ab4b269c7de3a87aa4a7c38ffa18601eb8d1',
   },
   {
-    message: 'Update3 main.yml',
+    message: 'Update main.yml',
+    authoredDate: '2019-09-19T00:07:39Z',
+    committedDate: '2019-09-19T00:07:39Z',
     author: {
       avatarUrl: 'https://avatars2.githubusercontent.com/u/4082254?v=4',
       user: {
@@ -26,11 +28,147 @@ export const commitData = [
       },
     },
     treeUrl:
-      'https://github.com/jstacoder/jstacoder.github.io/tree/98e9ab4b269c7de3a87aa4a7c38ffa18601eb8d1',
+      'https://github.com/jstacoder/jstacoder.github.io/tree/3a86d11025722feb9de1207b73e8ca76c0bf1b32',
     commitUrl:
-      'https://github.com/jstacoder/jstacoder.github.io/commit/98e9ab4b269c7de3a87aa4a7c38ffa18601eb8d1',
-    authoredDate: '2019-09-19T16:18:44Z',
-    committedDate: '2019-09-19T16:18:44Z',
-    commitSha: '98e9ab4b269c7de3a87aa4a7c38ffa18601eb8d1',
+      'https://github.com/jstacoder/jstacoder.github.io/commit/3a86d11025722feb9de1207b73e8ca76c0bf1b32',
+    commitSha: '3a86d11025722feb9de1207b73e8ca76c0bf1b32',
+  },
+  {
+    message: 'Update main.yml',
+    authoredDate: '2019-09-19T00:06:05Z',
+    committedDate: '2019-09-19T00:06:05Z',
+    author: {
+      avatarUrl: 'https://avatars2.githubusercontent.com/u/4082254?v=4',
+      user: {
+        userName: 'jstacoder',
+        userLink: 'https://github.com/jstacoder',
+      },
+    },
+    treeUrl:
+      'https://github.com/jstacoder/jstacoder.github.io/tree/ea1da6f122d651c5451d62e7dcc54cd069335006',
+    commitUrl:
+      'https://github.com/jstacoder/jstacoder.github.io/commit/ea1da6f122d651c5451d62e7dcc54cd069335006',
+    commitSha: 'ea1da6f122d651c5451d62e7dcc54cd069335006',
+  },
+  {
+    message:
+      "Merge branch 'develop' of github.com:jstacoder/jstacoder.github.io into develop",
+    authoredDate: '2019-09-19T00:04:22Z',
+    committedDate: '2019-09-19T00:04:22Z',
+    author: {
+      avatarUrl: 'https://avatars2.githubusercontent.com/u/4082254?v=4',
+      user: {
+        userName: 'jstacoder',
+        userLink: 'https://github.com/jstacoder',
+      },
+    },
+    treeUrl:
+      'https://github.com/jstacoder/jstacoder.github.io/tree/cf390772665187f927ffd2a41d1012c9f1c42d6e',
+    commitUrl:
+      'https://github.com/jstacoder/jstacoder.github.io/commit/cf390772665187f927ffd2a41d1012c9f1c42d6e',
+    commitSha: 'cf390772665187f927ffd2a41d1012c9f1c42d6e',
+  },
+  {
+    message: 'added patch-package',
+    authoredDate: '2019-09-19T00:03:58Z',
+    committedDate: '2019-09-19T00:03:58Z',
+    author: {
+      avatarUrl: 'https://avatars2.githubusercontent.com/u/4082254?v=4',
+      user: {
+        userName: 'jstacoder',
+        userLink: 'https://github.com/jstacoder',
+      },
+    },
+    treeUrl:
+      'https://github.com/jstacoder/jstacoder.github.io/tree/3519bfce5e3432901c4a2ec48618f73c5f7742a2',
+    commitUrl:
+      'https://github.com/jstacoder/jstacoder.github.io/commit/3519bfce5e3432901c4a2ec48618f73c5f7742a2',
+    commitSha: '3519bfce5e3432901c4a2ec48618f73c5f7742a2',
+  },
+  {
+    message: 'Update main.yml',
+    authoredDate: '2019-09-17T20:47:19Z',
+    committedDate: '2019-09-17T20:47:19Z',
+    author: {
+      avatarUrl: 'https://avatars2.githubusercontent.com/u/4082254?v=4',
+      user: {
+        userName: 'jstacoder',
+        userLink: 'https://github.com/jstacoder',
+      },
+    },
+    treeUrl:
+      'https://github.com/jstacoder/jstacoder.github.io/tree/ffe012d597f3e3cc0c8fb04f7a3359d6ef0f017a',
+    commitUrl:
+      'https://github.com/jstacoder/jstacoder.github.io/commit/ffe012d597f3e3cc0c8fb04f7a3359d6ef0f017a',
+    commitSha: 'ffe012d597f3e3cc0c8fb04f7a3359d6ef0f017a',
+  },
+  {
+    message:
+      "Merge branch 'develop' of github.com:jstacoder/jstacoder.github.io into develop",
+    authoredDate: '2019-09-17T19:26:28Z',
+    committedDate: '2019-09-17T19:26:28Z',
+    author: {
+      avatarUrl: 'https://avatars2.githubusercontent.com/u/4082254?v=4',
+      user: {
+        userName: 'jstacoder',
+        userLink: 'https://github.com/jstacoder',
+      },
+    },
+    treeUrl:
+      'https://github.com/jstacoder/jstacoder.github.io/tree/3d809a013c4df16dec9689d993cdc4361925e4e3',
+    commitUrl:
+      'https://github.com/jstacoder/jstacoder.github.io/commit/3d809a013c4df16dec9689d993cdc4361925e4e3',
+    commitSha: '3d809a013c4df16dec9689d993cdc4361925e4e3',
+  },
+  {
+    message: 'trying github_token fix',
+    authoredDate: '2019-09-17T19:26:09Z',
+    committedDate: '2019-09-17T19:26:09Z',
+    author: {
+      avatarUrl: 'https://avatars2.githubusercontent.com/u/4082254?v=4',
+      user: {
+        userName: 'jstacoder',
+        userLink: 'https://github.com/jstacoder',
+      },
+    },
+    treeUrl:
+      'https://github.com/jstacoder/jstacoder.github.io/tree/005348f74ae434781a5531693cb84f9bafd4332f',
+    commitUrl:
+      'https://github.com/jstacoder/jstacoder.github.io/commit/005348f74ae434781a5531693cb84f9bafd4332f',
+    commitSha: '005348f74ae434781a5531693cb84f9bafd4332f',
+  },
+  {
+    message: 'Update main.yml',
+    authoredDate: '2019-09-17T19:21:31Z',
+    committedDate: '2019-09-17T19:21:31Z',
+    author: {
+      avatarUrl: 'https://avatars2.githubusercontent.com/u/4082254?v=4',
+      user: {
+        userName: 'jstacoder',
+        userLink: 'https://github.com/jstacoder',
+      },
+    },
+    treeUrl:
+      'https://github.com/jstacoder/jstacoder.github.io/tree/525359ef301040712792fbd8423d19675bd92d64',
+    commitUrl:
+      'https://github.com/jstacoder/jstacoder.github.io/commit/525359ef301040712792fbd8423d19675bd92d64',
+    commitSha: '525359ef301040712792fbd8423d19675bd92d64',
+  },
+  {
+    message: 'Update main.yml\n\ntesting',
+    authoredDate: '2019-09-17T19:13:41Z',
+    committedDate: '2019-09-17T19:13:41Z',
+    author: {
+      avatarUrl: 'https://avatars2.githubusercontent.com/u/4082254?v=4',
+      user: {
+        userName: 'jstacoder',
+        userLink: 'https://github.com/jstacoder',
+      },
+    },
+    treeUrl:
+      'https://github.com/jstacoder/jstacoder.github.io/tree/bb7c08168b15185bf0c3d1656b263a3b666ccff5',
+    commitUrl:
+      'https://github.com/jstacoder/jstacoder.github.io/commit/bb7c08168b15185bf0c3d1656b263a3b666ccff5',
+    commitSha: 'bb7c08168b15185bf0c3d1656b263a3b666ccff5',
   },
 ]
